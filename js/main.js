7e57d7070e1db9131407561511df791351f85129
tezalve <tezalve@gmail.com>
2023-03-03 06:54:04 +0600

    :sparkles: getting date from api and showing

--- js/main.js
+++ js/main.js
@@ -38,7 +38,7 @@ const displayTech = (data,limit = 6) =>{
                     <hr/>
                     <div class="card-body">
                         <h5 class="card-title">${tech.name}</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <i class="bi bi-calendar-date"> ${tech.published_in}</i>
                     </div>
                 </div>
             `
