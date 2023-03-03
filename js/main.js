9cd2f6ada7810a220d266d196ae676d48b99a8d6
tezalve <tezalve@gmail.com>
2023-03-03 06:11:35 +0600

    :sparkles: features data showing correctly

--- js/main.js
+++ js/main.js
@@ -21,22 +21,43 @@ const displayTech = (data,limit = 6) =>{
     let count = 0;
     data.data.tools.every(tech => {
         if(count < limit){
-            // console.log(tech);
+
+            // show features in an oredered list(was there a better way?)
+            const listElements = [];
+            for(let i=0; i < tech.features.length; i++){
+                listElements[i] = `<li>${tech.features[i]}</li>`;
+            }
+            let featuresAll = listElements.join("");
+
             const techDiv = document.createElement('div');
             techDiv.classList.add('col');
             techDiv.innerHTML = `
                 <div class="card h-100">
                     <img src="${tech.image}" class="card-img-top" alt="">
                     <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <h5 class="card-title">Features</h5>
+                        <ol>
+                            ${featuresAll}
+                        </ol>
                     </div>
-                    <div class="card-footer">
-                        <small class="text-muted">Last updated 3 mins ago</small>
+                    <hr/>
+                    <div class="card-body">
+                        <h5 class="card-title">${tech.name}</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                     </div>
                 </div>
             `
             techContainer.appendChild(techDiv);
+
+            
+
+            // const listContainer = document.getElementById('olist');
+            // tech.features.forEach(feature => {
+            //     listContainer.innerHTML += `
+            //         <li>${feature}</li>
+            //     `
+            // });
+
             count++;
             return true;
         }
